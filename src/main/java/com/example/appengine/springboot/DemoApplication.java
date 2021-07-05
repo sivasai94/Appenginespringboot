f00c36255cf79f996786509cfc8254ac559ad4c7
siva <reddysivasai33@gmail.com>
2021-07-05 12:44:58 +0530

    First Commit

---
+++ src/main/java/com/example/appengine/springboot/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.appengine.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	  public String getMsgName() {
+	    return "Welcome siva!";
+	  }
+	@GetMapping("/hi")
+	  public String hello() {
+	    return "Hello";
+	  }
+	
+
+}
